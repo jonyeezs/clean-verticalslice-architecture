fcbf485f518bae5475a9ed7cb8e0b9bbab25da8c
Jonathan Yee <jonyeezs@gmail.com>
2023-04-25 22:11:16 +1000

    fix terraform cloud config

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "Whip-Up"
+    organization = "whip-up"
 
     workspaces {
       name = "csharp-clean-architecture-github-action"
