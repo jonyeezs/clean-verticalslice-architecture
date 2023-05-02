a70d164b64b0e397d9f57a2882092059a1890be0
Jonathan Yee <jonathan.yee@ellumehealth.com>
2023-05-02 23:07:55 +1000

    remove hope of it being HTTPS
    There's just no way for it to be https if i don't have my own custom domain

--- infrastructure/load-balancer.tf
+++ infrastructure/load-balancer.tf
@@ -4,39 +4,6 @@ resource "aws_lb" "this" {
   subnets         = data.aws_subnets.this.ids
 }
 
-# Remove this when when we have a proper DNS
-resource "aws_acm_certificate" "temporary" {
-  domain_name       = aws_lb.this.dns_name
-  validation_method = "DNS"
-
-  tags = {
-    Environment = "Temporary"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_lb_listener" "this" {
-  load_balancer_arn = aws_lb.this.arn
-
-  port            = "443"
-  protocol        = "HTTPS"
-  ssl_policy      = "ELBSecurityPolicy-2016-08"
-  certificate_arn = aws_acm_certificate.temporary.arn
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Sorry, we could not find the resource you were looking for. Please check your request and try again."
-      status_code  = "404"
-    }
-  }
-}
-
 resource "aws_lb_target_group" "this" {
   name        = "${local.service}-lb-tg"
   port        = 80
@@ -52,6 +19,42 @@ resource "aws_lb_target_group" "this" {
     timeout             = 10
     healthy_threshold   = 3
     unhealthy_threshold = 3
+  }
+}
+
+# TODO generate cert for HTTPS when we actually have a domain ready
+# resource "aws_acm_certificate" "temporary" {
+#   domain_name       = aws_lb.this.dns_name
+#   validation_method = "DNS"
+
+#   tags = {
+#     Environment = "Temporary"
+#   }
+
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.this.arn
+
+  port     = "80"
+  protocol = "HTTP"
+  # TODO make it HTTPS when we actually have a domain ready
+  # port            = "443"
+  # protocol        = "HTTPS"
+  # certificate_arn = aws_acm_certificate.temporary.arn
+  ssl_policy = "ELBSecurityPolicy-2016-08"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Sorry, we could not find the resource you were looking for. Please check your request and try again."
+      status_code  = "404"
+    }
   }
 }
 
