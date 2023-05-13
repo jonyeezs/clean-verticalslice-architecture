173f6c353ba8c74cdb5af69ec3be960ee68b6756
Jonathan Yee <jonathan.yee@ellumehealth.com>
2023-05-13 16:14:04 +1000

    infra: move init task definition to json file
    Not sure if this changes anything but i'm just trying to see what sticks now

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -4,23 +4,14 @@ resource "aws_ecs_cluster" "this" {
 
 resource "aws_ecs_task_definition" "initiator" {
   family             = "${local.service}-task"
+  network_mode       = "awsvpc"
   task_role_arn      = aws_iam_role.task.arn
   execution_role_arn = aws_iam_role.task.arn
 
-  container_definitions = jsonencode(
-    [{
-      "name" : local.service
+  container_definitions = jsonencode([
+    {
+      "name" : "clean-slice",
       "image" : "httpd:2.4",
-      "essential" : true,
-      "logConfiguration" : {
-        "logDriver" : "awslogs",
-        "options" : {
-          "awslogs-region" : "${local.region}",
-          "awslogs-stream-prefix" : "${local.service}",
-          "awslogs-group" : "${local.service}-task",
-          "awslogs-create-group" : "true"
-        }
-      },
       "portMappings" : [
         {
           "containerPort" : 80,
@@ -28,20 +19,29 @@ resource "aws_ecs_task_definition" "initiator" {
           "protocol" : "tcp"
         }
       ],
+      "logConfiguration" : {
+        "logDriver" : "awslogs",
+        "options" : {
+          "awslogs-group" : "clean-slice-task",
+          "awslogs-region" : "ap-southeast-2",
+          "awslogs-stream-prefix" : "clean-slice",
+          "awslogs-create-group" : "true"
+        }
+      },
+      "essential" : true,
       "entryPoint" : [
         "sh",
         "-c"
       ],
       "command" : [
-        "/bin/sh -c \"echo '<html> <head> <title>Clean slice</title> <style>body {margin-top: 40px; background-color: #333;} </style> </head><body> <div style=color:white;text-align:center> <h2>Congratulations!</h2> <p>Time to deploy your app!</p> </div></body></html>' >  /usr/local/apache2/htdocs/index.html && httpd-foreground\""
-      ],
-    }]
-  )
-  cpu    = 256
-  memory = 512
+        "/bin/sh -c \"echo '<html> <head> <title>Temporary page</title> <style>body {margin-top: 40px; background-color: #333;} </style> </head><body> <div style=color:white;text-align:center> <h2>Congratulations!</h2> <p>Now ready for CICD.</p> </div></body></html>' >  /usr/local/apache2/htdocs/index.html && httpd-foreground\""
+      ]
+    }
+  ])
 
   requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
 }
 
 resource "aws_cloudwatch_log_group" "task" {
