b9b6f3024f57afe6849251e06ccbd46aa32440a5
Jonathan Yee <jonyeezs@gmail.com>
2024-10-05 00:41:16 +1000

    just adding more validation to the create recipe request

--- dotnet/src/Api/UseCases/CreateRecipe/Validator.cs
+++ dotnet/src/Api/UseCases/CreateRecipe/Validator.cs
@@ -6,6 +6,10 @@ namespace CleanSlice.Api.UseCases.CreateRecipe
     {
         public Validator()
         {
+            RuleFor(x => x.Url)
+                .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _))
+                .When(x => !string.IsNullOrEmpty(x.Url))
+                .WithMessage("Url ({PropertyValue}) is not valid");
             RuleFor(x => x.Title).NotEmpty();
         }
     }
